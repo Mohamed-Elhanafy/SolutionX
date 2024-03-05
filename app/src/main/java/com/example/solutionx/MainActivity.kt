705a2ddc899121d91922eb6d14087b5278626bd9
Mohamed-Elhanafy <amohamed8684@gmail.com>
2024-03-05 06:38:05 +0200

    adding navigation to the project

--- app/src/main/java/com/example/solutionx/MainActivity.kt
+++ app/src/main/java/com/example/solutionx/MainActivity.kt
@@ -6,17 +6,24 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.solutionx.utils.Logger
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button: Button
-    lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
 
         val locale = Locale.getDefault()
         val language = locale.language
@@ -30,17 +37,17 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        button = findViewById<Button>(R.id.changeLanguage).apply {
+/*        button = findViewById<Button>(R.id.changeLanguage).apply {
             typeface?.let { setTypeface(it) }
         }
         text = findViewById<TextView>(R.id.textView).apply {
             typeface?.let { setTypeface(it) }
-        }
+        }*/
 
 
 
         val logger = Logger(this)
-        logger.log("Activity created")
+        logger.log("home" , "Activity created")
 
     }
 }
