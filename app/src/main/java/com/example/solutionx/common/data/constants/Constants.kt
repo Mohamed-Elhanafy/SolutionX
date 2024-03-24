3b7b372140658613ca55da1404857c2cf997e145
Mohamed-Elhanafy <amohamed8684@gmail.com>
2024-03-24 22:15:24 +0200

    adding NetworkModule

---
+++ app/src/main/java/com/example/solutionx/common/data/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.solutionx.common.data.constants
+
+object Constants {
+
+    const val BASE_URL = "https://dev.api.altashirat.solutionplus.net/api/"
+}
---
+++ app/src/main/java/com/example/solutionx/common/data/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.example.solutionx.common.data.di
+
+import android.content.Context
+import com.example.solutionx.common.data.constants.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): LoginApi =
+        Retrofit.Builder()
+            .addConverterFactory(gsonConverterFactory)
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build().create(LoginApi::class.java)
+
+}
