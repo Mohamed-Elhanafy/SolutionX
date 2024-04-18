125104562b907c9cc0326b866599b69992cf1c3c
Mohamed-Elhanafy <amohamed8684@gmail.com>
2024-04-18 09:04:16 +0200

    test RemoteDataSource

---
+++ app/src/test/java/com/example/solutionx/features/login/data/repositoty/remote/FakeRestApiNetworkProvider.kt
@@ -0,0 +1,39 @@
+package com.example.solutionx.features.login.data.repositoty.remote
+
+import com.example.solutionx.common.domain.repository.remote.IRestApiNetworkProvider
+import java.lang.reflect.Type
+
+class FakeRestApiNetworkProvider : IRestApiNetworkProvider {
+
+    var postResponse: Any? = null
+
+    override suspend fun <ResponseBody> get(
+        responseWrappedModel: Type,
+        pathUrl: String,
+        headers: Map<String, Any>?,
+        queryParameters: Map<String, Any>?
+    ): ResponseBody {
+        throw UnsupportedOperationException("Not implemented")
+    }
+
+    override suspend fun <ResponseBody, RequestBody> post(
+        responseWrappedModel: Type,
+        pathUrl: String,
+        headers: Map<String, Any>?,
+        queryParameters: Map<String, Any>?,
+        requestBody: RequestBody?
+    ): ResponseBody {
+        @Suppress("UNCHECKED_CAST")
+        return postResponse as ResponseBody
+    }
+
+    override suspend fun <ResponseBody, RequestBody> put(
+        responseWrappedModel: Type,
+        pathUrl: String,
+        headers: Map<String, Any>?,
+        queryParameters: Map<String, Any>?,
+        requestBody: RequestBody?
+    ): ResponseBody {
+        throw UnsupportedOperationException("Not implemented")
+    }
+}
