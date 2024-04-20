2e76b6b9d0ed3fb8eab50abdaf6cfcece9055cb8
Mohamed-Elhanafy <amohamed8684@gmail.com>
2024-04-20 13:46:48 +0200

    change the android.util.Base64 to java.util.Base64 to test it easily

--- app/src/main/java/com/example/solutionx/features/login/data/repositoty/local/LocalDataSource.kt
+++ app/src/main/java/com/example/solutionx/features/login/data/repositoty/local/LocalDataSource.kt
@@ -1,60 +1,60 @@
 package com.example.solutionx.features.login.data.repositoty.local
 
 import am.leon.utilities.android.helpers.logging.LoggerFactory
-import android.util.Base64
+import com.example.solutionx.android.helpers.security.IKeystoreUtils
 import com.example.solutionx.android.helpers.security.KeystoreUtils
 import com.example.solutionx.features.login.data.model.entity.LoginResponseEntity
 import com.example.solutionx.features.login.data.model.entity.UserEntity
 import com.example.solutionx.common.domain.repository.local.KeyValueStorage
 import com.example.solutionx.features.login.domain.repository.local.ILocalDataSource
 import com.google.gson.Gson
+import java.util.Base64
 
 internal class LocalDataSource(
     private val keyValueStorage: KeyValueStorage,
-    private val gson: Gson
+    private val gson: Gson,
+    private val keystoreUtils: IKeystoreUtils
 ) : ILocalDataSource {
 
 
 
     override suspend fun saveLogin(loginResponse: LoginResponseEntity) {
         val userJson = gson.toJson(loginResponse.user)
-        val (userIv, encryptedUser) = KeystoreUtils.encrypt(userJson)
-        val encryptedUserString = Base64.encodeToString(encryptedUser, Base64.DEFAULT)
+        val (userIv, encryptedUser) = keystoreUtils.encrypt(userJson)
+        val encryptedUserString = Base64.getEncoder().encodeToString(encryptedUser)
 
         // Save the encrypted user string and its IV in the datastore
         keyValueStorage.save(USER_ENCRYPTED, encryptedUserString)
-        keyValueStorage.save(USER_IV, Base64.encodeToString(userIv, Base64.DEFAULT))
+        keyValueStorage.save(USER_IV, Base64.getEncoder().encodeToString(userIv))
 
-        val (accessTokenIv, encryptedAccessToken) = KeystoreUtils.encrypt(loginResponse.accessToken)
-        val encryptedAccessTokenString = Base64.encodeToString(encryptedAccessToken, Base64.DEFAULT)
+        val (accessTokenIv, encryptedAccessToken) = keystoreUtils.encrypt(loginResponse.accessToken)
+        val encryptedAccessTokenString = Base64.getEncoder().encodeToString(encryptedAccessToken)
 
         // Save the encrypted access token string and its IV in the datastore
         keyValueStorage.save(ACCESS_TOKEN_ENCRYPT, encryptedAccessTokenString)
-        keyValueStorage.save(ACCESS_TOKEN_IV, Base64.encodeToString(accessTokenIv, Base64.DEFAULT))
+        keyValueStorage.save(ACCESS_TOKEN_IV, Base64.getEncoder().encodeToString(accessTokenIv))
 
         keyValueStorage.save(IS_USER_LOGGED_IN, true.toString())
-
-        logger.info("Login saved")
     }
 
     override suspend fun getAccessToken(): String {
         val encryptedAccessTokenString = keyValueStorage.get<String, String>(ACCESS_TOKEN_ENCRYPT)
         val accessTokenIvString = keyValueStorage.get<String, String>(ACCESS_TOKEN_IV)
 
-        val encryptedAccessToken = Base64.decode(encryptedAccessTokenString, Base64.DEFAULT)
-        val accessTokenIv = Base64.decode(accessTokenIvString, Base64.DEFAULT)
+        val encryptedAccessToken = Base64.getDecoder().decode(encryptedAccessTokenString)
+        val accessTokenIv = Base64.getDecoder().decode(accessTokenIvString)
 
-        return KeystoreUtils.decrypt(accessTokenIv, encryptedAccessToken)
+        return keystoreUtils.decrypt(accessTokenIv, encryptedAccessToken)
     }
 
     override suspend fun getUser(): UserEntity {
         val encryptedUserString = keyValueStorage.get<String, String>(USER_ENCRYPTED)
         val userIvString = keyValueStorage.get<String, String>(USER_IV)
 
-        val encryptedUser = Base64.decode(encryptedUserString, Base64.DEFAULT)
-        val userIv = Base64.decode(userIvString, Base64.DEFAULT)
+        val encryptedUser = Base64.getDecoder().decode(encryptedUserString)
+        val userIv = Base64.getDecoder().decode(userIvString)
 
-        val userJson = KeystoreUtils.decrypt(userIv, encryptedUser)
+        val userJson = keystoreUtils.decrypt(userIv, encryptedUser)
         return gson.fromJson(userJson, UserEntity::class.java)
     }
 
